1c7f4964761a15e1f8bda0960aefb58ed11686a4
Eldar Singin <eldarsingin@gmail.com>
2020-03-31 21:49:26 +0300

    MatrixTraversalByDiagonals refactoring - replaced String with StringBuilder

--- src/main/java/com/algorithms/MatrixTraversalByDiagonals.java
+++ src/main/java/com/algorithms/MatrixTraversalByDiagonals.java
@@ -3,27 +3,28 @@ package com.algorithms;
 public class MatrixTraversalByDiagonals {
 
     public String traverse(final int R, int C, int[][] matrix) {
-        String results = "";
+        StringBuilder results = new StringBuilder();
         for (int i = 0; i < R; i++) {
-            results += extractDiagonal(i, 0, C, matrix);
+            results.append(extractDiagonal(i, 0, C, matrix));
         }
 
         for (int j = 1; j < C; j++) {
-            results += extractDiagonal(R - 1, j, C, matrix);
+            results.append(extractDiagonal(R - 1, j, C, matrix));
         }
 
-        return results.trim();
+        return results.toString().trim();
     }
 
-    private String extractDiagonal(final int i, final int j, final int C, final int[][] matrix) {
+    private StringBuilder extractDiagonal(final int i, final int j, final int C, final int[][] matrix) {
         int x = i;
         int y = j;
-        String result = "";
+        StringBuilder result = new StringBuilder();
         while (x >= 0 && y < C ) {
             if((i + j) % 2 == 0){
-                result = matrix[x][y] + " " + result;
+                result.insert(0, matrix[x][y] + " ");
             } else {
-                result += matrix[x][y] + " ";
+                result.append(matrix[x][y]);
+                result.append(" ");
             }
             x--;
             y++;
