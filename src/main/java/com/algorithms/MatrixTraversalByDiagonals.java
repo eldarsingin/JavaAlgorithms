239beb993b0e63527c4173bd9c910a7f6433d5eb
Eldar Singin <eldarsingin@gmail.com>
2020-03-31 21:34:05 +0300

    MatrixTraversalByDiagonals refactoring

--- src/main/java/com/algorithms/MatrixTraversalByDiagonals.java
+++ src/main/java/com/algorithms/MatrixTraversalByDiagonals.java
@@ -15,42 +15,31 @@ public class MatrixTraversalByDiagonals {
         List<Integer> results = new ArrayList<>();
         boolean reverse = true;
         for (int i = 0; i < R; i++) {
-            int x = i;
-            int y = 0;
-
-            List<Integer> diagonal = new ArrayList<>();
-            while (y < C && x >= 0) {
-                diagonal.add(matrix[x][y]);
-                x--;
-                y++;
-            }
-            if (reverse) {
-                Collections.reverse(diagonal);
-            }
-            results.addAll(diagonal);
+            results.addAll(extractDiagonal(reverse, i, 0, C, matrix));
             reverse = !reverse;
         }
 
         for (int j = 1; j < C; j++) {
-            int x = R - 1;
-            int y = j;
-
-            List<Integer> diagonal = new ArrayList<>();
-            while (y < C && x >= 0) {
-                diagonal.add(matrix[x][y]);
-                x--;
-                y++;
-            }
-
-            if (reverse) {
-                Collections.reverse(diagonal);
-            }
-            results.addAll(diagonal);
+            results.addAll(extractDiagonal(reverse, R - 1, j, C, matrix));
             reverse = !reverse;
         }
 
         return results.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(" "));
+    }
+
+    private List<Integer> extractDiagonal(boolean reverse, int x, int y, int C, int[][] matrix) {
+        List<Integer> diagonal = new ArrayList<>();
+        while (x >= 0 && y < C ) {
+            if(!reverse){
+                diagonal.add(matrix[x][y]);
+            } else {
+                diagonal.add(0, matrix[x][y]);
+            }
+            x--;
+            y++;
+        }
+        return diagonal;
     }
 }
